95689a1569f86e0863f689b2d8015a500d981fe1
zizfn <1803942+zizifn@users.noreply.github.com>
2023-05-06 15:59:28 +0800

    add sh

--- start.sh
+++ start.sh
@@ -6,4 +6,13 @@ if [ ! -f "./xray/xray" ]; then
     exit 1
 fi
 
+# check file nohup.out exist and rm it
+if [ -f "./nohup.out" ]; then
+    rm -rf ./nohup.out
+fi
+
+# find process ./xray/xray and kill it
+ps -ef | grep "./xray/xray" | grep -v grep | awk '{print $2}' | xargs kill -9
+
 # run ./xray/xray in backgroud
+nohup ./xray/xray -c server.json &
